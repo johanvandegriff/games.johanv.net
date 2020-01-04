485894e2880aa34e82ecca0ed77799162d33146a
Johan Vandegriff <johan@vandymail.com>
2020-01-04 07:35:33 -0500

    ui tweaks, new table sorting that does numbers correctly

---
+++ static/sorttable2.js
@@ -0,0 +1,78 @@
+// https://www.w3schools.com/howto/howto_js_sort_table.asp
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
+
+function sortTable(id, n, isNumber) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById(id);
+    switching = true;
+    //Set the sorting direction to descending:
+    dir = "desc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+
+        /*Extract the innermost element from nested tags*/
+        while(x.children.length > 0) { x = x.children[0]; }
+        while(y.children.length > 0) { y = y.children[0]; }
+
+        /*check if the two rows should switch place,
+        based on the direction, asc or desc:*/
+        if (isNumber) {
+          if (dir == "asc") {
+            if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch= true;
+              break;
+            }
+          } else if (dir == "desc") {
+            if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        } else {
+          if (dir == "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch= true;
+              break;
+            }
+          } else if (dir == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              //if so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
